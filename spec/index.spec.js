0db7dce11198894f1d74acc7f47fdd979582a223
markj82 <markjohn666@gmail.com>
2019-05-27 21:41:47 +0100

    second test for sum

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -9,6 +9,13 @@ describe('sum', () => {
     const expected = 7;
     expect(actual).to.equal(expected);
   });
+  it('returns sum of two given numbers, second example', () => {
+    const a = 23;
+    const b = 2;
+    const actual = sum(a, b);
+    const expected = 25;
+    expect(actual).to.equal(expected);
+  });
 });
 
 describe('minus', () => {
