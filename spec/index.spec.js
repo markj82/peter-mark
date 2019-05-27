dac92ac3c413f87f6f03a1fb44861ff5ccf3bc0c
Anuradis <p.pachocki@gmail.com>
2019-05-27 21:33:25 +0100

    minus function index.spec.js

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -1,12 +1,25 @@
-const { expect } = require('chai');
-const sum = require('../index');
+const {expect} = require('chai');
+const {sum, minus} = require('../index');
 
 describe('sum', () => {
-    it('returns sum of two given numbers', () => {
-        const a = 3;
-        const b = 4;
-        const actual = sum(a, b);
-        const expected = 7;
-        expect(actual).to.equal(expected);
-    });
+  it('returns sum of two given numbers', () => {
+    const a = 3;
+    const b = 4;
+    const actual = sum(a, b);
+    const expected = 7;
+    expect(actual).to.equal(expected);
+  });
+});
+
+describe('minus', () => {
+  it('returns sum of two given numbers', () => {
+
+
+    const a = 5;
+    const b = 10;
+    const actual = minus(a, b);
+    const expected = -5;
+    expect(actual).to.equal(expected);
+
+  });
 });
